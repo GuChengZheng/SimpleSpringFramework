918f5096a0c83653b81961835935d5e061547f17
zhenggucheng <zhenggucheng@linklogis.com>
2019-01-02 20:29:29 +0800

    修改Factory的init

--- src/main/java/MySpring/Utils/Factory.java
+++ src/main/java/MySpring/Utils/Factory.java
@@ -1,7 +1,5 @@
 package MySpring.Utils;
 
-import org.springframework.context.annotation.Bean;
-
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
@@ -13,15 +11,28 @@ import java.util.Map;
  * @create: 2018-12-23 23-07
  **/
 public class Factory {
-    private Map<String, BeanInstance> singleBeans = new HashMap<>();
+    /**
+     * 存储作用域为singleton的bean集合（默认情况下为singleton）
+     */
+    private static Map<String, BeanInstance> singletonBeans = new HashMap<>();
+
+    private static  Map<String, BeanInstance> prototypeBeans = new HashMap<>();
 
     public void init(List<BeanInstance> beans){
-        beans.stream().forEach(bean -> {singleBeans.put(bean.getId(), bean);});
-
+        beans.stream().forEach(bean -> {
+            switch (bean.getScopeType()){
+                case singleton:
+                    singletonBeans.put(bean.getId(), bean);
+                    break;
+                case prototype:
+                    prototypeBeans.put(bean.getId(), bean);
+                    break;
+            }
+        });
     }
 
     public Object getBean(String id) throws Exception{
-        BeanInstance bean = singleBeans.get(id);
+        BeanInstance bean = singletonBeans.get(id);
         Class clazz = Class.forName(bean.getClassName());
         Object object = clazz.newInstance();
         for(Map.Entry<String, String> entry : bean.getPropertyMap().entrySet()) {
